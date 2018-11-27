155ad7f1ac1771a02181b2793131fc4387563a6e
Your Name <you@example.com>
2018-11-27 06:47:19 +0000

    update avatar

--- src/utils/api.js
+++ src/utils/api.js
@@ -124,6 +124,29 @@ const authRequest = async (options, showLoading = true) => {
   return request(options, showLoading)
 }
 
+const updateFile = async (options = {}) => {
+  // 显示loading
+  wepy.showLoading({title: '上传中'})
+
+  // 获取 token
+  let accessToken = await getToken()
+
+  // 拼接url
+  options.url = host + '/' + options.url
+  let header = options.header || {}
+  // 将 token 设置在 header 中
+  header.Authorization = 'Bearer ' + accessToken
+  options.header = header
+
+  // 上传文件
+  let response = await wepy.uploadFile(options)
+
+  // 隐藏 loading
+  wepy.hideLoading()
+
+  return response
+}
+
 //  退出登录
 const logout = async (params = {}) => {
   let accessToken = wepy.getStorageSync('access_token')
@@ -150,5 +173,6 @@ export default {
   authRequest,
   refreshToken,
   login,
-  logout
+  logout,
+  updateFile
 }
